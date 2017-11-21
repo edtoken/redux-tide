27276731506bd2723238b6f032f0d7f9aff9fe04
edtoken <editied@gmail.com>
2017-11-21 19:58:02 +0300

    del old tests

--- test/action.spec.js
+++ test/action.spec.js
@@ -1,38 +1,9 @@
 import 'should'
 
 import {
-  _defaultResponseMapper,
   _makeActionHandler,
   _makeActionUniqId
 } from '../src/action'
-
-describe('action _defaultResponseMapper ', function() {
-  it('_defaultResponseMapper returns simple object', function() {
-    const data = {
-      data: {}
-    }
-    const result = _defaultResponseMapper(data)
-    result.should.be.deepEqual(data)
-  })
-
-  it('_defaultResponseMapper returned nested', function() {
-    const data = {
-      data: {
-        key: {
-          key: {
-            key: {
-              key: {
-                key: 'value'
-              }
-            }
-          }
-        }
-      }
-    }
-    const result = _defaultResponseMapper(data)
-    result.should.be.deepEqual(data)
-  })
-})
 
 describe('action _makeActionUniqId ', function() {
   it('_makeActionUniqId returns uniquie ids', function() {
